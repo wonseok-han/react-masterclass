736d50244705606b35aa770e20dc822e53c5c1e2
wonseok-han <oshan1112@gmail.com>
2022-06-04 16:24:45 +0900

    styled-components alias and attrs

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,31 @@ const Circle = styled(Box)`
   border-radius: 50px;
 `;
 
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+// Component Attribute
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
+`;
+
 function App() {
   return (
     <Father>
-      <Box color="teal" />
-      <Circle color="tomato" />
+      {/* <Box color="teal" />
+      <Circle color="tomato" /> */}
+      <Btn>Log in</Btn>
+      {/* Component alias */}
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
+      <Input />
+      <Input />
+      <Input required={true} />
     </Father>
   );
 }
