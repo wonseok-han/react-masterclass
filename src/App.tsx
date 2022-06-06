7f924aebf3e6e9f63c4dd5ca9d226f2f62118658
wonseok-han <oshan1112@gmail.com>
2022-06-06 15:35:24 +0900

    typescript forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import Circle from 'components/Circle';
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('');
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+
+    console.log(value);
+    setUsername(value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(username);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
