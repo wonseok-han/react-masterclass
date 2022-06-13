1ee263a7e180f28d22b09db8c13ca2a510157f27
wonseok-han <oshan1112@gmail.com>
2022-06-13 21:37:10 +0900

    state Recoil Selector

--- todo/src/atoms.ts
+++ todo/src/atoms.ts
@@ -1,7 +1,20 @@
+import { atom, selector } from 'recoil';
+
 import { TodoProps } from './components/types';
-import { atom } from 'recoil';
 
 export const toDoState = atom<Array<TodoProps>>({
   key: 'toDo',
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: 'toDoSelector',
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((todo) => todo.category === 'TO_DO'),
+      toDos.filter((todo) => todo.category === 'DOING'),
+      toDos.filter((todo) => todo.category === 'DONE'),
+    ];
+  },
 });
--- todo/src/components/TodoList.tsx
+++ todo/src/components/TodoList.tsx
@@ -2,21 +2,36 @@
 
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
-import { toDoState } from 'atoms';
+import { toDoSelector } from 'atoms';
 import { useRecoilValue } from 'recoil';
 
 const TodoList = () => {
-  const toDos = useRecoilValue(toDoState);
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
 
-  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <hr />
+      <h2>To Do</h2>
       <ul>
         {toDos.map((todo) => (
           <ToDo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((doing) => (
+          <ToDo key={doing.id} {...doing} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((done) => (
+          <ToDo key={done.id} {...done} />
         ))}
       </ul>
     </div>
