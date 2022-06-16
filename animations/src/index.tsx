9dd2449a6e1932f6285570b9868d1f92e530847c
wonseok-han <oshan1112@gmail.com>
2022-06-16 22:17:26 +0900

    settings update

--- animations/src/index.tsx
+++ animations/src/index.tsx
@@ -1,7 +1,9 @@
-import App from './App';
+/* eslint-disable import/default */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
+
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
