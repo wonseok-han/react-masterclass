da2ba5630fc7e6a932cb65db6c81746397787c13
wonseok-han <oshan1112@gmail.com>
2022-06-16 22:21:47 +0900

    test

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -25,33 +25,24 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: {
-    scale: 0,
-  },
-  end: {
-    scale: 1,
-    rotateZ: 360,
-    transition: {
-      type: 'spring',
-      bounce: 0.5,
-      duration: 3,
-      delay: 0.5,
-    },
-  },
-};
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <Wrapper>
-        <Box variants={myVars} initial="start" animate="end" />
+        <Box />
       </Wrapper>
     </ThemeProvider>
   );
