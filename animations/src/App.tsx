55578492e9a4201e5b45460b418fb818a09633f2
wonseok-han <oshan1112@gmail.com>
2022-06-16 23:13:30 +0900

    animations gestures deep

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 import { darkTheme, reset } from 'styles/global';
@@ -22,10 +23,20 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* overflow: hidden; */
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -33,33 +44,33 @@ const Box = styled(motion.div)`
 
 const boxVariants = {
   hover: {
-    scale: 1.5,
+    scale: 1,
     rotateZ: 90,
   },
   click: {
     scale: 1,
     borderRadius: '100px',
   },
-  drag: {
-    backgroundColor: 'rgb(46, 204, 113)',
-    transition: {
-      duration: 10,
-    },
-  },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <Wrapper>
-        <Box
-          drag
-          variants={boxVariants}
-          whileHover="hover"
-          whileDrag={'drag'}
-          whileTap="click"
-        ></Box>
+        <BiggerBox ref={biggerBoxRef}>
+          <Box
+            drag
+            dragSnapToOrigin
+            dragElastic={0.5}
+            dragConstraints={biggerBoxRef}
+            variants={boxVariants}
+            whileHover="hover"
+            whileTap="click"
+          />
+        </BiggerBox>
       </Wrapper>
     </ThemeProvider>
   );
