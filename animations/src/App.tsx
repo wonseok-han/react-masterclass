7cace8b716cf04a868b6e8c86974ccf9213ac9d9
wonseok-han <oshan1112@gmail.com>
2022-06-16 22:44:57 +0900

    atnimations variants deep

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -25,8 +25,10 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 10px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -34,15 +36,51 @@ const Circle = styled(motion.div)`
   background-color: white;
   height: 70px;
   width: 70px;
+  place-self: center;
+  border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: 'spring',
+      duration: 1,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <Wrapper>
-        <Box />
+        <Box variants={boxVariants} initial="start" animate="end">
+          <Circle variants={circleVariants} />
+          <Circle variants={circleVariants} />
+          <Circle variants={circleVariants} />
+          <Circle variants={circleVariants} />
+        </Box>
       </Wrapper>
     </ThemeProvider>
   );
